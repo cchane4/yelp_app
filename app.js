7919b720d960f9ec7c2710c6b4170164d3b89b8a
Chad Chaney <cchane4@gmail.com>
2019-11-01 16:37:09 -0500

    adding mongoose, a database and reworking the post route and form

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 const express = require("express"); 
 const app = express(); 
 const body_parser = require ("body-parser"); 
+const mongoose = require("mongoose"); 
 
-/*taken out of app.get so that post route can push new campgrounds into the array
-and to have access to it in the post route */
-let campgrounds_values = [ 
-    {name:"Salmon Creek", image:"https://pixabay.com/get/57e2d1424253ab14f6da8c7dda793f7f1636dfe2564c704c722e7cdd934fc45c_340.jpg"}, 
-    {name:"Walnut Hill", image:"https://pixabay.com/get/54e5d4454f5aae14f6da8c7dda793f7f1636dfe2564c704c722e7cdd934fc45c_340.jpg"}, 
-    {name:"Cherry Mounds", image:"https://pixabay.com/get/53e2dc434b51b108f5d084609620367d1c3ed9e04e50744e742872d0954cc1_340.jpg"},
-    {name:"Salmon Creek", image:"https://pixabay.com/get/57e2d1424253ab14f6da8c7dda793f7f1636dfe2564c704c722e7cdd934fc45c_340.jpg"}, 
-    {name:"Walnut Hill", image:"https://pixabay.com/get/54e5d4454f5aae14f6da8c7dda793f7f1636dfe2564c704c722e7cdd934fc45c_340.jpg"}, 
-    {name:"Cherry Mounds", image:"https://pixabay.com/get/53e2dc434b51b108f5d084609620367d1c3ed9e04e50744e742872d0954cc1_340.jpg"},
-    {name:"Salmon Creek", image:"https://pixabay.com/get/57e2d1424253ab14f6da8c7dda793f7f1636dfe2564c704c722e7cdd934fc45c_340.jpg"}, 
-    {name:"Walnut Hill", image:"https://pixabay.com/get/54e5d4454f5aae14f6da8c7dda793f7f1636dfe2564c704c722e7cdd934fc45c_340.jpg"}, 
-    {name:"Cherry Mounds", image:"https://pixabay.com/get/53e2dc434b51b108f5d084609620367d1c3ed9e04e50744e742872d0954cc1_340.jpg"}
-                  ]; 
 
-app.use(body_parser.urlencoded({extended: true})); 
+mongoose.connect('mongodb://localhost/yelp_camp'); 
+app.use(body_parser.urlencoded({extended: true })); 
 app.set("view engine", "ejs"); 
 
+let campground_Schema = new mongoose.Schema({ 
+  name: String, 
+  image: String
+}); 
+
+let Campground = mongoose.model("Campground", campground_Schema); 
 app.get ("/", (req, res) => { 
     res.render("landing");
 });
+
+// Campground.create({ 
+//     name:"Walnut Hill", 
+//     image:"https://pixabay.com/get/54e5d4454f5aae14f6da8c7dda793f7f1636dfe2564c704c722e7cdd934fc45c_340.jpg"
+    
+// },(err, campground) => { 
+//     if (err){ 
+//         console.log (err); 
+//     } else { 
+//         console.log("newly created campground"); 
+//         console.log(campground); 
+//     }
+// }); 
 
 // houses the form that sends the data to the post route
 app.get("/campgrounds/new", (req, res) => { 
@@ -29,8 +37,14 @@ app.get("/campgrounds/new", (req, res) => {
 
 })
 app.get ("/campgrounds", (req, res) => { 
-
-res.render("campgrounds", {campgrounds_key: campgrounds_values});
+// get all campgrounds from DB 
+  Campground.find({}, (err, all_campgrounds) => { 
+    if(err){ 
+        console.log(err);
+    } else { 
+    res.render("campgrounds", {campgrounds_key: all_campgrounds});
+    }
+  });
 });
 
 //this is a different route than the app.get campground
@@ -38,10 +52,16 @@ app.post("/campgrounds", (req, res) => {
     //get data from form and add to campgrounds array
     let name = req.body.name; 
     let image = req.body.image; 
+    // make a new variable as a separate step that combines the name and image in one object
     let new_campground = { name: name, image: image}; 
-    campgrounds_values.push(new_campground); 
-    //redirect back to same app.get campgrounds page
-    res.redirect("/campgrounds"); 
+    Campground.create(new_campground, (err, newly_created) => { 
+        if (err) { 
+        console.log(err); 
+        } else { 
+        //redirect back to the campgrounds page and should see a new entry
+        res.redirect("/campgrounds"); 
+        }
+    });
 }); 
 
 app.listen(3000, () => { 
