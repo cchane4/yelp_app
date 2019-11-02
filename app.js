7c8dbadff94c0aac7591aa3f6c81f21fa4543709
Chad Chaney <cchane4@gmail.com>
2019-11-01 21:53:57 -0500

    adding a show page, route and way to get data from the database to show one campground

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.set("view engine", "ejs");
 
 let campground_Schema = new mongoose.Schema({ 
   name: String, 
-  image: String
+  image: String, 
+  description: String
 }); 
 
 let Campground = mongoose.model("Campground", campground_Schema); 
@@ -18,31 +19,46 @@ app.get ("/", (req, res) => {
     res.render("landing");
 });
 
-// Campground.create({ 
-//     name:"Walnut Hill", 
-//     image:"https://pixabay.com/get/54e5d4454f5aae14f6da8c7dda793f7f1636dfe2564c704c722e7cdd934fc45c_340.jpg"
+Campground.create({ 
+    name:"Walnut Hill", 
+    image:"https://pixabay.com/get/54e5d4454f5aae14f6da8c7dda793f7f1636dfe2564c704c722e7cdd934fc45c_340.jpg", 
+    description: "this is a huge campground"
     
-// },(err, campground) => { 
-//     if (err){ 
-//         console.log (err); 
-//     } else { 
-//         console.log("newly created campground"); 
-//         console.log(campground); 
-//     }
-// }); 
+},(err, campground) => { 
+    if (err){ 
+        console.log (err); 
+    } else { 
+        console.log("newly created campground"); 
+        console.log(campground); 
+    }
+}); 
 
 // houses the form that sends the data to the post route
 app.get("/campgrounds/new", (req, res) => { 
     res.render("new.ejs"); 
 
-})
+}); 
+
+// SHOW- shows more info about one campground
+app.get("/campgrounds/:id", (req, res) => { 
+// find the campground with provided ID
+Campground.findById(req.params.id, (err, found_campground) => { 
+    if(err){ 
+        console.log(err); 
+    } else { 
+    //render show template with that campground 
+      res.render("show",{campground: found_campground}); 
+    }
+  }); 
+}); 
+
 app.get ("/campgrounds", (req, res) => { 
 // get all campgrounds from DB 
   Campground.find({}, (err, all_campgrounds) => { 
     if(err){ 
         console.log(err);
     } else { 
-    res.render("campgrounds", {campgrounds_key: all_campgrounds});
+    res.render("index", {campgrounds_key: all_campgrounds});
     }
   });
 });
@@ -50,10 +66,11 @@ app.get ("/campgrounds", (req, res) => {
 //this is a different route than the app.get campground
 app.post("/campgrounds", (req, res) => { 
     //get data from form and add to campgrounds array
-    let name = req.body.name; 
-    let image = req.body.image; 
+    let camp_name = req.body.name; 
+    let camp_image = req.body.image; 
+    let camp_desc = req.body.description; 
     // make a new variable as a separate step that combines the name and image in one object
-    let new_campground = { name: name, image: image}; 
+    let new_campground = { name: camp_name, image: camp_image, description: camp_desc}; 
     Campground.create(new_campground, (err, newly_created) => { 
         if (err) { 
         console.log(err); 
